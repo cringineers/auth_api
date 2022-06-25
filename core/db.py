6850896fe2d64c27c65ec86148f69c49e6d5fd1f
GarnikOriginal <gavid19912@gmail.com>
2022-06-25 11:49:03 +0500

    Fix error on auth failed

--- core/db.py
+++ core/db.py
@@ -5,4 +5,4 @@ async def check_user_data(connection_params, user, password):
     connection = await asyncpg.connect(**connection_params)
     query = "select id from tag_system.users where login = $1 and hash = $2"
     user_id = await connection.fetchrow(query, user, password)
-    return user_id != [], user_id[0] if user_id != [] else None
+    return user_id is not None, user_id[0] if user_id is not None else None
